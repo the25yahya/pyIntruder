6ba62f441a39809afa9fba08efd0418e3c9cca8c
the25yahya <yahyaraja.2@gmail.com>
2024-12-10 18:39:17 -0500

    added stdin and readfile

--- intruder.py
+++ intruder.py
@@ -1,18 +1,17 @@
 from library.parser import *
-import requests
 from library.engine import *
 from library.lib import *
 import argparse
 import threading
-
+import sys
 
 
 if __name__ == '__main__':
 
-
     parser = argparse.ArgumentParser(description="Example script")
     parser.add_argument('-t', '--threads', type=int, help="Number of threads")
     parser.add_argument('-w', '--wordlist', type=str, help="Path to the wordlist file",required=True)
+    parser.add_argument('-f', '--file', type=str, help="Path to the requestfile")
     parser.add_argument('-M', '--mode',default='fuzzing', type=str, help='''
     intruder execution mode : 
     for simple fuzzing ignore this arg (simple fuzzing is the default mode)
@@ -30,9 +29,13 @@ if __name__ == '__main__':
     threads_arg = args.threads
     wordlist = args.wordlist
     choice = args.mode
+    if not sys.stdin.isatty():
+        request = read_stdin()
+    elif args.file:
+        request = read_file(args.file)
 
     if choice =='fuzzing':
-        request = accept_req()
+        #request = accept_req()
         req = Request(request)
         engine = Engine(wordlist)
         threads = []
--- library/lib.py
+++ library/lib.py
@@ -1,5 +1,4 @@
-
-
+import sys
 
 def accept_req():
         print("\033[1;31m[*] Paste the HTTP request(use FUZZ as a placeholder for the sentence you want to fuzz), end with an empty line:\033[0m")
@@ -9,4 +8,15 @@ def accept_req():
             if line == "":
                 break
             user_input.append(line)
-        return "\n".join(user_input)  # Join lines into a single string    
+        return "\n".join(user_input)  
+
+def read_stdin():
+    req = []
+    for line in sys.stdin:
+        req.append(line.strip())  
+    return "\n".join(req)  
+
+def read_file(file_path):
+    with open(file_path,'r') as file :
+         request = file.read()
+    return request
